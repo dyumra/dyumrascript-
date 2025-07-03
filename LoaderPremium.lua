afba6f2527efe5bcd0f1ffad28a02179589ac664
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-03 13:19:39 +0700

    Update LoaderPremium.lua

--- LoaderPremium.lua
+++ LoaderPremium.lua
@@ -1,1 +1,8 @@
+local validKey = "DYHUB-ZZ77-1KSLI-602C-PEWUG-2M5O"
+
+if getgenv().Key == validKey then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/DYHUB-Universal/refs/heads/main/Key1%2B1.lua"))()
+else
+    warn("Invalid or missing Key. Script will not run.")
+end
 
