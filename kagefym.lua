4dc0d683ec9837970a6433445db7a30584b8c854
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-04 00:16:33 +0700

    Update kagefym.lua

--- kagefym.lua
+++ kagefym.lua
@@ -1,2 +1,2 @@
-getgenv().Key = "DYHUB-7K3V9-MN2X4-PQ8Z1-R5T7B-W0F6L"
+getgenv().Key = "DYHUB-7K3V9-MN2X4-PQ8Z1-R5T7B-W0F6L-LIFETIME"
 loadstring(game:HttpGet('https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/7days.lua'))()
