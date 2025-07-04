5f1ee058cdd1257df998e7a325a5829f2b0641ea
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-04 12:19:13 +0700

    Update yavib-lifetime.lua

--- yavib-lifetime.lua
+++ yavib-lifetime.lua
@@ -1,2 +1,2 @@
 getgenv().Key = "DYHUB-8JY5A-Q3RTW-IK7OL-FRJL2-IPL7W-LIFETIME"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/7days.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/yavib.lua'))()
