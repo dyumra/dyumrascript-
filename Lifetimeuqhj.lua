6ff60b44ced2e13990cf7437e3e3f6b6916b32e6
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-06 16:25:37 +0700

    Create Lifetimeuqhj.lua

---
+++ Lifetimeuqhj.lua
@@ -0,0 +1,2 @@
+getgenv().Key = "DYHUB-JYHAG-UYWBH-ERQ4T-OIAH7-PI9K8-LIFETIME"
+loadstring(game:HttpGet('https://raw.githubusercontent.com/dyumra/dyumrascript-/refs/heads/main/YMH012-Lifetime.lua'))()
