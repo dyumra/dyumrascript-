e49ac6798247136f97aa5a1f2b4b9823f364e2dd
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-06 16:36:25 +0700

    Update Donot123hahaky.lua

--- Donot123hahaky.lua
+++ Donot123hahaky.lua
@@ -1,23 +1,32 @@
 local validKey = "DYHUB-JYHAG-UYWBH-ERQ4T-OIAH7-PI9K8-LIFETIME"
-local allowedUsername = "Donot123haha"
-local allowedUsername2 = "YMH012"
+local allowedUsernames = { "Donot123haha", "YMH012" }
+
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
 local StarterGui = game:GetService("StarterGui")
 
-if not player or player.Name ~= allowedUsername2 then
-    StarterGui:SetCore("SendNotification", {
-        Title = "Access Denied",
-        Text = "The first user must reset HWID before proceeding.",
-        Duration = 5,
-    })
+local function isAllowedUsername(name)
+    for _, allowedName in pairs(allowedUsernames) do
+        if name == allowedName then
+            return true
+        end
+    end
+    return false
+end
+
+if not player or not isAllowedUsername(player.Name) then
+    StarterGui:SetCore("SendNotification", {
+        Title = "Access Denied",
+        Text = "The first user must reset HWID before proceeding.",
+        Duration = 5,
+    })
 elseif getgenv().Key ~= validKey then
-    StarterGui:SetCore("SendNotification", {
-        Title = "Invalid Key",
-        Text = "Your key is invalid or missing. The script will not run.",
-        Duration = 5,
-    })
+    StarterGui:SetCore("SendNotification", {
+        Title = "Invalid Key",
+        Text = "Your key is invalid or missing. The script will not run.",
+        Duration = 5,
+    })
 else
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/DYHUB-Universal/refs/heads/main/Key1%2B1.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/dyumra/DYHUB-Universal/refs/heads/main/Key1%2B1.lua"))()
 end
