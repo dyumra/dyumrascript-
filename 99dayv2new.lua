25482c212655b1a98ca0d9db4e342a542adbe46c
dyumra (DYHUB) <woraphan8871234@gmail.com>
2025-07-26 21:13:40 +0700

    Update and rename 99dayv2test.lua (1).txt to 99dayv2new.lua

--- 99dayv2new.lua
+++ 99dayv2new.lua
@@ -134,13 +134,13 @@ Window:EditOpenButton({
 })
 
 local Tabs = {
-    Main = Window:Tab({ Title = "Main", Icon = "star" }),
-    Teleport = Window:Tab({ Title = "Teleport", Icon = "rocket" }),
+    Main = Window:Tab({ Title = "Main", Icon = "rocket" }),
+    Teleport = Window:Tab({ Title = "Teleport", Icon = "map-pin" }),
     Player = Window:Tab({ Title = "Player", Icon = "user" }),
     Esp = Window:Tab({ Title = "Esp", Icon = "eye" }),
     Bring = Window:Tab({ Title = "Bring Items", Icon = "package" }),
     Hitbox = Window:Tab({ Title = "Hitbox", Icon = "target" }),
-    Misc = Window:Tab({ Title = "Misc", Icon = "tool" }),
+    Misc = Window:Tab({ Title = "Misc", Icon = "file-cog" }),
 }
 
 local infHungerActive = false
